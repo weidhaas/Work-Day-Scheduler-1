637f21ec1b96fdad40e4594d75bcce6210586128
sc <108869274+weidhaas@users.noreply.github.com>
2022-08-13 12:20:42 -0500

    add time tables

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+const d = new Date();
+document.getElementById("currentDay").innerHTML = d.toDateString();
+console.log(today.toLocaleDateString("en-US", options))
+
+
+// variables
+$(document).ready(function() {
+
+
+
+    // functions
+    function handleSaveTask() {
+      // get the text
+      let textValue = $(this).siblings('.task').val();
+  
+      // get the time
+      let timeValue = $(this).parent().attr('data-time');
+  
+      console.log(textValue);
+      console.log(timeValue);
+  
+      // save textarea text to local storage
+      localStorage.setItem(timeValue, textValue);
+  
+    }
+  
+    // color the row based on hour 
+    function colorRows() {
+      // current hour
+      const currentHour = moment().hours();
+      console.log(currentHour);
+      // loop through rows add color class based on time
+      $('.js-row').each(function() {
+        const rowHour = parseInt($(this).attr('data-time'));
+        if (rowHour < currentHour) {
+          $(this).addClass('gray');
+        } else if (rowHour === currentHour) {
+          $(this).addClass('red');
+        } else {
+          $(this).addClass('green');
+        }
+      })
+    }
+  
+    // events
+    // save btn event
+    $('#nine').on('click', handleSaveTask);
+    $('#ten').on('click', handleSaveTask);
+  
+    // runs on page load
+    // populate textarea with localstorage
+  
+    // loop through rows and add a class (gray, red, green)
+    colorRows();
+  
+  });
