6bd6801d033bce3942a4951de3e6d29374409ddf
sc <108869274+weidhaas@users.noreply.github.com>
2022-08-17 13:08:11 -0500

    update to js and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,49 @@
-
 //date 
 
 const d = moment().format("dddd, MMMM Do, YYYY");
 document.getElementById("currentDay").innerHTML = d;
 
+// variables
+$(document).ready(function() {
 
 
-//returning storage
+
+    // functions
+    function handleSaveTask() {
+      console.log('hi from save')
+      // get the text
+      let textValue = $(this).siblings('.description').children('.task').val();
+  
+      // get the time
+      let timeValue = $(this).parent().attr('data-time');
+  
+      console.log(textValue);
+      console.log(timeValue);
+  
+      // save textarea text to local storage
+      localStorage.setItem(timeValue, textValue);
+  
+    }
+  
+    // color the row based on hour 
+    function colorRows() {
+      // current hour
+      const currentHour = moment().hours();
+      console.log(currentHour);
+      // loop through rows add color class based on time
+      $('.row').each(function() {
+        const rowHour = parseInt($(this).attr('data-time'));
+        if (rowHour < currentHour) {
+          $(this).addClass('gray');
+        } else if (rowHour === currentHour) {
+          $(this).addClass('red');
+        } else {
+          $(this).addClass('green');
+        }
+      })
+    }
+
+    //returning storage
 $("#time9 .description").val(localStorage.getItem("9"));
 $("#time10 .description").val(localStorage.getItem("10"));
 $("#time11 .description").val(localStorage.getItem("11"));
@@ -19,10 +56,54 @@ $("#time17 .description").val(localStorage.getItem("5"));
 $("#time18 .description").val(localStorage.getItem("6"));
 $("#time19 .description").val(localStorage.getItem("7"));
 $("#time20 .description").val(localStorage.getItem("8"));
-$("#time21 .description").val(localStorage.getItem("9"));
+$("#time21 .description").val(localStorage.getItem("9p"));
+  
 
-$('button').on('click', setTasks)
 
+
+    // events
+    // save btn event
+    $('#nineam').on('click', handleSaveTask);
+    $('#tenam').on('click', handleSaveTask);
+    $('#elevenam').on('click', handleSaveTask);
+    $('#twelvepm').on('click', handleSaveTask);
+    $('#onepm').on('click', handleSaveTask);
+    $('#twopm').on('click', handleSaveTask);
+    $('#threepm').on('click', handleSaveTask);
+    $('#fourpm').on('click', handleSaveTask);
+    $('#fivepm').on('click', handleSaveTask);
+    $('#sixpm').on('click', handleSaveTask);
+    $('#sevenpm').on('click', handleSaveTask);
+    $('#eightpm').on('click', handleSaveTask);
+    $('#ninepm').on('click', handleSaveTask);
+
+  
+  
+    // runs on page load
+    // populate textarea with localstorage
+    $("#time9 .task").val(localStorage.getItem("9"));
+    $("#time10 .task").val(localStorage.getItem("10"));
+    $("#time11 .task").val(localStorage.getItem("11"));
+    $("#time12 .task").val(localStorage.getItem("12"));
+    $("#time13 .task").val(localStorage.getItem("1"));
+    $("#time14 .task").val(localStorage.getItem("2"));
+    $("#time15 .task").val(localStorage.getItem("3"));
+    $("#time16 .task").val(localStorage.getItem("4"));
+    $("#time17 .task").val(localStorage.getItem("5"));
+    $("#time18 .task").val(localStorage.getItem("6"));
+    $("#time19 .task").val(localStorage.getItem("7"));
+    $("#time20 .task").val(localStorage.getItem("8"));
+    $("#time21 .task").val(localStorage.getItem("9p"));
+    
+  
+    
+
+
+    // loop through rows and add a class (gray, red, green)
+    colorRows();
+  
+  });
+/*
 function setTasks(a) {
   $(this).find().removeClass().addClass();
 
@@ -173,4 +254,4 @@ const tasktest = document.getElementById("nineam");
     // loop through rows and add a class (gray, red, green)
     colorRows();
   
-  });
+  
